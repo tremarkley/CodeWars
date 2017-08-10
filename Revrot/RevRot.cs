a8cf8c8ee65defdffc6a616796b5114a2194f504
Harry Markley <tremarkley@gmail.com>
2017-08-10 10:05:52 -0700

    Add files via upload

---
+++ Revrot/RevRot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revrot
+{
+    public class Revrot
+    {
+        public static string RevRot(string strng, int sz)
+        {
+            if (String.IsNullOrEmpty(strng) || sz <= 0)
+            {
+                return "";
+            }
+            else if (sz > strng.Length)
+            {
+                return "";
+            }
+            else
+            {
+                int numberOfChunks = strng.Length / sz;
+                string finalString = string.Empty;
+                for (int i = 0; i < numberOfChunks; i++)
+                {
+                    int startingPosition = i * sz;
+                    string substr = strng.Substring(startingPosition, sz);
+                    double sumOfCubesofDigits = 0;
+                    //for (int j = 0; j < substr.Length; j++)
+                    foreach(char c in substr)
+                    {
+                        sumOfCubesofDigits += Math.Pow(int.Parse(c.ToString()), 3);
+                    }
+                    if (sumOfCubesofDigits % 2 == 0)
+                    {
+                        //reverse
+                        char[] charArray = substr.ToCharArray();
+                        Array.Reverse(charArray);
+                        substr = new string(charArray);
+                    }
+                    else
+                    {
+                        string firstChar = substr.Substring(0, 1);
+                        substr = substr.Substring(1) + firstChar;
+                    }
+                    finalString += substr;
+                }
+                return finalString;
+            }
+        }
+    }
+}
