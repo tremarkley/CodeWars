678f43ee9d58bdd925ee3eda2ec579181d66c3cc
Harry Markley <tremarkley@gmail.com>
2017-08-21 16:45:28 -0700

    Added .sln and GoodvsEvil Project

--- CodeWars/CodeWars/Program.cs
+++ CodeWars/CodeWars/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Revrot
+namespace CodeWars
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Revrot.RevRot("123456779", 8));
-            Console.ReadLine();
         }
     }
 }
--- CodeWars/CodeWars/Properties/AssemblyInfo.cs
+++ CodeWars/CodeWars/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Revrot")]
+[assembly: AssemblyTitle("CodeWars")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Revrot")]
+[assembly: AssemblyProduct("CodeWars")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c193442-a5cc-4104-b657-bc2af1fa77c3")]
+[assembly: Guid("537bfa48-092a-40e2-9d33-c412aaf30dc4")]
 
 // Version information for an assembly consists of the following four values:
 //
